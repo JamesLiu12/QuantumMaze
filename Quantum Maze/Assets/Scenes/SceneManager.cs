6ffaf633f3dbd9f1391f9f74017c39cd1f0287f6
陈湛明 <1670732206@qq.com>
2022-12-14 12:40:59 +0000

    修改主菜单
    菜单增加两项；
    添加返回按钮。

--- Quantum Maze/Assets/Scenes/SceneManager.cs
+++ Quantum Maze/Assets/Scenes/SceneManager.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    public void LoadMenu()
+    public void Load_Menu()
         => UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Menu");
 
-    public void LoadMain()
-        => UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Main");
+    public void Load_QuantumStepByStep()
+        => UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Quantum (step by step)");
 
-    public void LoadSmoothMain()
-        => UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/MainSmooth");
+    public void Load_QuantumAuto()
+        => UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Quantum (auto)");
+
+    public void Load_ClassicalBFS()
+        => UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Classical BFS");
+
+    public void Load_ClassicalDFS()
+        => UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Classical DFS");
 }
